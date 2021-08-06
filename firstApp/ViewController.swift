f2966c3a2251ea05f9d574c0220dbc6feded0926
Semih <31005189+sualtindakiadam@users.noreply.github.com>
2021-08-06 11:31:19 +0300

    Initial Commit

---
+++ firstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by Semih Kalaycı on 6.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
