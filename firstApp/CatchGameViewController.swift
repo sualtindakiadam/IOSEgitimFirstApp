8b4ced08fc475c2b096cd94ab0fde0a16b9fa338
Semih <31005189+sualtindakiadam@users.noreply.github.com>
2021-08-11 11:40:56 +0300

    first commit

---
+++ firstApp/CatchGameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CatchGameViewController.swift
+//  firstApp
+//
+//  Created by Semih Kalaycı on 10.08.2021.
+//
+
+import UIKit
+
+class CatchGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ firstApp/JestAppViewController.swift
@@ -0,0 +1,29 @@
+//
+//  JestAppViewController.swift
+//  firstApp
+//
+//  Created by Semih Kalaycı on 10.08.2021.
+//
+
+import UIKit
+
+class JestAppViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
